834b025935ffd92b0929026a60e0ed4949e731f3
HeydarQa <qahaydar@gmail.com>
2020-08-18 21:01:28 -0400

    Summer2020_B20

--- src/day43_Static/CybertekStudent.java
+++ src/day43_Static/CybertekStudent.java
@@ -1,4 +1,25 @@
 package day43_Static;
+class A{
+    static int a=200;
+        }
 
 public class CybertekStudent {
+    // for the adventages of status
+
+
+ String studentName;
+ int age;
+ char gender;
+
+ static  String schoolName="Cybertek School";
+
+ public  void getInfo(){
+     System.out.println("School name: "+schoolName);
+
+
+ }
+public static  void printSchoolName(){
+    System.out.println("School name: "+schoolName);
+}
+
 }
