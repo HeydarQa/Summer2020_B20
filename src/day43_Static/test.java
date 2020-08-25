2f010f00f369365eee7a0b032e17836d0bc8a344
HeydarQa <qahaydar@gmail.com>
2020-08-25 00:10:14 -0400

    Day43

--- src/day43_Static/test.java
+++ src/day43_Static/test.java
@@ -1,11 +1,23 @@
 package day43_Static;
 
+import java.util.Arrays;
+
+import static  Library.Resources.a;
+
 public class test {
     public static void main(String[] args) {
         System.out.println(StaticBlock3.a);
         System.out.println(StaticBlock3.b);
         System.out.println(StaticBlock3.c);
 
+        System.out.println(Arrays.toString(Data.arr));
+        System.out.println(Data.list);
+        System.out.println("---------------------------------------------");
+        System.out.println();
+
+        System.out.println(a);
+
 
     }
+
 }
